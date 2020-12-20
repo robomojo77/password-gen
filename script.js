98dce9628acb74a7dc3271b0b70641ca9dd0b702
Chris Bomm <wordsciencechris@gmail.com>
2020-12-20 12:51:15 -0600

    entirely new logic model

--- script.js
+++ script.js
@@ -2,6 +2,26 @@
 
 // Generate Password Function
 
+// VARIABLES
+
+var passwordLower = "n";
+var passwordUpper = "n";
+var passwordNumeric = "n";
+var passwordSpecial = "n";
+var passwordLength = 0;
+var currentType = 0;
+
+// Strings
+
+var passwordString = "";
+var lowerCharacters = "abcdefghijklmnopqrstuvwxyz";
+var upperCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericCharacters = "1234567890";
+var specialCharacters = "!#$%&+/*?";
+var allowedCharacters = "";
+
+// GLOBAL FUNCTIONS
+
 function forceLowercase(currentCharacter) {
   if (currentCharacter === "N") {
     return 'n';
@@ -11,127 +31,142 @@ function forceLowercase(currentCharacter) {
   }
 }
 
+function addLower() {
+  var randomLowerNumber = (Math.random() * lowerCharacters.length);
+  var randomLowerCharacter = lowerCharacters.charAt(randomLowerNumber);
+  return randomLowerCharacter;
+}
+
+function addUpper() {
+  var randomUpperNumber = (Math.random() * upperCharacters.length);
+  var randomUpperCharacter = upperCharacters.charAt(randomUpperNumber);
+  return randomUpperCharacter;
+}
+
+function addNumeric() {
+  var randomNumericNumber = (Math.random() * numericCharacters.length);
+  var randomNumericCharacter = numericCharacters.charAt(randomNumericNumber);
+  return randomNumericCharacter;
+}
+
+function addSpecial() {
+  var randomSpecialNumber = (Math.random() * specialCharacters.length);
+  var randomSpecialCharacter = specialCharacters.charAt(randomSpecialNumber);
+  return randomSpecialCharacter;
+}
+
+function generateCharacter() {
+  var randomAllowedNumber = (Math.random() * allowedCharacters.length);
+  var randomAllowedCharacter = allowedCharacters.charAt(randomAllowedNumber);
+  return randomAllowedCharacter;
+  }
+
+
+
 function generatePassword() {
 
-// Password Length
-passwordLength = 0; 
-do {
-  passwordLength = window.prompt("How long should this password be? (must be between 8-128 characters)")
-}
-while (passwordLength < 8 | passwordLength > 128)
+  // Password Length - User inputs desired length. Test to see it meets req.
 
-// Character Types
-
-do {
-  // lowercase
-
-  passwordLower = "n";
   do {
-    passwordLower = window.prompt("Include lowercase characters? y or n (must choose at least one type)")
-    passwordLower = forceLowercase(passwordLower);
+    passwordLength = window.prompt("How long should this password be? (must be between 8-128 characters)")
   }
-  while (passwordLower != "n" && passwordLower != "y")
-    
-  // uppercase
-  passwordUpper ="n"
+  while (passwordLength < 8 | passwordLength > 128)
+
+  // Character Types - User selects the required character types via y and n questions.
+
   do {
-    passwordUpper = window.prompt("Include uppercase characters? y or n (must choose at least one type)")
-    passwordUpper = forceLowercase(passwordUpper);
+    // lowercase included?
+
+    do {
+      passwordLower = window.prompt("Include lowercase characters? y or n (must choose at least one type)")
+
+      if (passwordLower === "N" || passwordLower === "Y") {
+        passwordLower = forceLowercase(passwordLower);
+      }
+
+    }
+    while (passwordLower != "n" && passwordLower != "y")
+
+    // uppercase included?
+
+    do {
+      passwordUpper = window.prompt("Include uppercase characters? y or n (must choose at least one type)")
+
+      if (passwordUpper === "N" || passwordUpper === "Y") {
+        passwordUpper = forceLowercase(passwordUpper);
+      }
+
+    }
+    while (passwordUpper != "n" && passwordUpper != "y")
+
+    // numeric included?
+
+    do {
+      passwordNumeric = window.prompt("Include numeric characters? y or n (must choose at least one type)")
+
+      if (passwordNumeric === "N" || passwordNumeric === "Y") {
+        passwordNumeric = forceLowercase(passwordNumeric);
+      }
+
+    }
+    while (passwordNumeric != "n" && passwordNumeric != "y")
+
+    // special included?
+
+    do {
+      passwordSpecial = window.prompt("Include special characters? y or n (must choose at least one type)")
+
+      if (passwordSpecial === "N" || passwordSpecial === "Y") {
+        passwordSpecial = forceLowercase(passwordSpecial);
+      }
+
+    }
+    while (passwordSpecial != "n" && passwordSpecial != "y")
+
   }
-  while (passwordUpper != "n" && passwordUpper != "y")
-    
-  // numeric
-  
-  passwordNumeric ="n"
-  do {
-    passwordNumeric = window.prompt("Include numeric characters? y or n (must choose at least one type)")
-    passwordNumeric = forceLowercase(passwordNumeric);
+  while (passwordLower === "n" && passwordUpper === "n" && passwordNumeric === "n" && passwordSpecial === "n")
+
+  // Apply Rules/Required Characters
+
+  if (passwordLower === "y") {
+    let newString = allowedCharacters + lowerCharacters;
+    allowedCharacters = newString;
+    let newString2 = passwordString + addLower();
+    passwordString = newString2;
   }
-  while (passwordNumeric != "n" && passwordNumeric != "y")
-    
-  // special
-  
-  passwordSpecial ="n"
-  do {
-    passwordSpecial = window.prompt("Include special characters? y or n (must choose at least one type)")
-    passwordSpecial = forceLowercase(passwordSpecial);
+
+  if (passwordUpper === "y") {
+    let newString = allowedCharacters + upperCharacters;
+    allowedCharacters = newString;
+    let newString2 = passwordString + addUpper();
+    passwordString = newString2;
   }
-  while (passwordSpecial != "n" && passwordSpecial != "y")
-    
-  // Create Password
 
-  // Build Character Selection String
+  if (passwordNumeric === "y") {
+    let newString = allowedCharacters + numericCharacters;
+    allowedCharacters = newString;
+    let newString2 = passwordString + addNumeric();
+    passwordString = newString2;
+  }
 
-  characterSelection = ""
+  if (passwordSpecial === "y") {
+    let newString = allowedCharacters + specialCharacters;
+    allowedCharacters = newString;
+    let newString2 = passwordString + addSpecial();
+    passwordString = newString2;
+  }
 
+// Fill remaining character slots
+  var remainingSlots = passwordLength - passwordString.length
 
+  console.log(remainingSlots)
 
-  // Lowercase Yes
-
-    // Uppercase Yes
-
-      // Numeric Yes
-
-        // Special Yes
-
-        // Special No
-
-      // Numeric No
-
-        // Special Yes
-
-        // Special No
-
-    // Uppercase No
-
-      // Numeric Yes
-
-        // Special Yes
-
-        // Special No
-
-      // Numeric No
-
-        // Special Yes
-
-        // Special No
-
-  
-  // Lowercase No
-
-    // Uppercase Yes
-
-      // Numeric Yes
-
-        // Special Yes
-
-        // Special No
-
-      // Numeric No
-
-        // Special Yes
-
-        // Special No
-
-    // Uppercase No
-
-      // Numeric Yes
-
-        // Special Yes
-
-        // Special No
-
-      // Numeric No
-
-        // Special Yes
-
-
-}
-while (passwordLower != "y" && passwordUpper != "y" && passwordNumeric != "y" && passwordSpecial != "y")
-
-
-
-
+  for (var i = 0; i < remainingSlots ; i++) {
+    let newString = passwordString + generateCharacter();
+    passwordString = newString;
+  }
+  console.log(allowedCharacters);
+  return passwordString;
 }
 
 // Get references to the #generate element
